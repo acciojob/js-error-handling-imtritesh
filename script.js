82e024048309c9fbb76e11ffb33946a4db738362
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-11 14:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-//your code here
 class OutOfRangeError extends Error {
-  constructor(arg) {
-    super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
+  constructor(message) {
+    super(message);
     this.name = 'OutOfRangeError';
   }
 }
 
 class InvalidExprError extends Error {
-  constructor() {
-    super('Expression should not have an invalid combination of expression');
+  constructor(message) {
+    super(message);
     this.name = 'InvalidExprError';
   }
 }
 
-function evalString(str) {
-  try {
-    if (/^\s*[+/*]/.test(str)) {
-      throw new SyntaxError('Expression should not start with invalid operator');
-    }
-    if (/[+/*-]\s*$/.test(str)) {
-      throw new SyntaxError('Expression should not end with invalid operator');
-    }
-    if (/[+/*-]{2}/.test(str)) {
-      throw new InvalidExprError();
-    }
-    const result = eval(str);
-    if (!Number.isInteger(result)) {
-      throw new OutOfRangeError(result);
-    }
-    return result;
-  } catch (err) {
-    if (err instanceof OutOfRangeError || err instanceof InvalidExprError) {
-      throw err;
-    }
-    throw new OutOfRangeError();
+function evalString(expr) {
+  // Check for invalid combination of operators
+  if (/(\+\+|\-\-|\+\-|\-\+|\*\/|\/\*|\+\*|\-\*|\*\+|\*\-)/.test(expr)) {
+    throw new InvalidExprError('Expression should not have an invalid combination of operators');
   }
+
+  // Check for invalid starting and ending operators
+  if (/^[+\/*]/.test(expr)) {
+    throw new SyntaxError('Expression should not start with an invalid operator');
+  }
+  if (/[\+\/*-]$/.test(expr)) {
+    throw new SyntaxError('Expression should not end with an invalid operator');
+  }
+
+  // Check for out of range characters
+  if (!/^[\d+\-\/*\s]+$/.test(expr)) {
+    throw new OutOfRangeError('Expression should only consist of integers and +-/* characters');
+  }
+
+  // Evaluate the expression
+  return eval(expr);
+}
+
+// Test the evalString function
+try {
+  console.log(evalString('1 + 2 * 3')); // Output: 7
+  console.log(evalString('1 + 2 - 3 / 4')); // Output: 2.25
+  console.log(evalString('10 * -2 + 5')); // Output: -15
+  console.log(evalString('2 ** 3')); // Throws OutOfRangeError
+  console.log(evalString('1++2')); // Throws InvalidExprError
+  console.log(evalString('*2+3')); // Throws SyntaxError
+  console.log(evalString('1-2/')); // Throws SyntaxError
+} catch (error) {
+  console.error(error.name + ': ' + error.message);
 }
